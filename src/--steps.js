43965afbad057c2fce305b8f9bc6488f464956b9
Shohagh019 <shohaghmunna@gmail.com>
2023-05-08 22:12:03 +0600

    first commit

---
+++ src/--steps.js
@@ -0,0 +1,18 @@
+/**
+ * Context API
+ * 1. create context and export
+ * 2. create provider 
+ * 3. set context value
+ * 4. to access the value use context hook
+ * **/
+
+
+/** for share context api
+ * 1. main.jsx => set AuthProvider
+ * 2. inside provider access children props and then use
+ * 
+ **
+/**
+ * 1. break down the create user function
+ * 2. auth provider will have auth and pass email and password from register
+ * **/
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAc_fHztYZMn1BnUIylRfVxUXXRKb-jxDA",
+  authDomain: "auth-firebase-context-ta-beef0.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-beef0",
+  storageBucket: "auth-firebase-context-ta-beef0.appspot.com",
+  messagingSenderId: "291245641364",
+  appId: "1:291245641364:web:0a2efd9b66bbc85718ae81",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
